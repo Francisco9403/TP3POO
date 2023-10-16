01fa59f56419858a452142aa0a92738fcccb7203
Fran <martinezfrancisco934@gmail.com>
2023-10-16 18:18:55 -0300

    Mi primer commit

---
+++ src/main/java/ar/edu/unnoba/poo2023/model/TP3POO/Tp3PooApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo2023.model.TP3POO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3PooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3PooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unnoba/poo2023/model/TP3POO/Tp3PooApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo2023.model.TP3POO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3PooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
